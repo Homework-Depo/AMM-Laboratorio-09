4b584246f7ce96e557f8192d56fd624da835b0c9
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-05-20 17:35:55 -0500

    First Commit

---
+++ laboratorio09/laboratorio09/PanDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace laboratorio09
+{	
+	public partial class PanDemo : ContentPage
+	{	
+		public PanDemo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ laboratorio09/laboratorio09/PinchDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace laboratorio09
+{	
+	public partial class PinchDemo : ContentPage
+	{	
+		public PinchDemo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
